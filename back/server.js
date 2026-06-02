8e24e84afb810f36a0ce0d46979eebe10af6c075
AlexanderM1927 <jhon71937@gmail.com>
2026-06-02 15:30:35 -0500

    ollama  running

--- back/server.js
+++ back/server.js
@@ -7,8 +7,22 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Backend Express funcionando' })
+app.get('/', async (req, res) => {
+  const ollamaUrl = process.env.OLLAMA_URL
+
+  const response = await fetch(`${ollamaUrl}/api/generate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      model: 'gemma3',
+      prompt: 'Hola',
+      stream: false
+    })
+  })
+  const data = await response.json()
+  res.json({ message: 'Backend Express funcionando', data })
 })
 
 const PORT = process.env.PORT || 8004
