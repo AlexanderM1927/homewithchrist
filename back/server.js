e9c69d0e807f72dc94ea1fc5ca1849d54ee6ebe5
AlexanderM1927 <jhon71937@gmail.com>
2026-06-02 15:20:33 -0500

    add dotenv

--- back/server.js
+++ back/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
@@ -10,6 +11,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Backend Express funcionando' })
 })
 
-app.listen(8004, '0.0.0.0', () => {
-  console.log('Backend corriendo en http://localhost:8004')
+const PORT = process.env.PORT || 8004
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend corriendo en http://localhost:${PORT}`)
 })
