e37e49535f76bd2426f3b48940d8aabf81d60426
AlexanderM1927 <jhon71937@gmail.com>
2026-06-04 20:56:57 -0500

    fix

--- back/server.js
+++ back/server.js
@@ -34,7 +34,7 @@ app.use('/api/training', trainingRoutes)
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
