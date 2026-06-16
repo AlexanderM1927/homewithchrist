164b87d31e99bbc0746c6b72364a199a44927d95
AlexanderM1927 <jhon71937@gmail.com>
2026-06-16 15:29:47 -0500

    increase limits on memory

--- back/ecosystem.config.cjs
+++ back/ecosystem.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       script: './dist/server.js',
       instances: 1,
       exec_mode: 'fork',
+      max_memory_restart: '500M',
       env: {
         NODE_ENV: 'production'
       }
--- back/models/index.js
+++ back/models/index.js
@@ -11,8 +11,8 @@ const sequelize = new Sequelize(
     dialect: process.env.DB_CONNECTION || 'mysql',
     logging: false,
     pool: {
-      max: 5,
-      min: 0,
+      max: 20,
+      min: 2,
       acquire: 30000,
       idle: 10000
     }
