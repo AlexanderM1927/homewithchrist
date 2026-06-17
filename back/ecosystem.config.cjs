2023f54d65637bde6af35220595fc644b189ae94
AlexanderM1927 <jhon71937@gmail.com>
2026-06-17 11:30:11 -0500

    increase memory restart

--- back/ecosystem.config.cjs
+++ back/ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       script: './dist/server.js',
       instances: 1,
       exec_mode: 'fork',
-      max_memory_restart: '500M',
+      max_memory_restart: '700M',
       env: {
         NODE_ENV: 'production'
       }
